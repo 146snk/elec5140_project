fff7f8b098f6d0e27c9810eace6376c6464f0bce
146snk <91668969+146snk@users.noreply.github.com>
2024-04-27 20:37:37 +0800

    Initial Modifictation
    Modified clock cycle count, tested different RAW and Load-use hazards.

--- RV32i/RV32i.sim/sim_1/behav/xsim/xsim.dir/Top_behav/obj/xsim_1.c
+++ RV32i/RV32i.sim/sim_1/behav/xsim/xsim.dir/Top_behav/obj/xsim_1.c
@@ -198,6 +198,7 @@ extern SYSTEMCLIB_IMP_DLLSPEC char** xsim_argv_copy ;
 int main(int argc, char **argv)
 {
     iki_heap_initialize("ms", "isimmm", 0, 2147483648) ;
+    iki_set_xsimdir_location_if_remapped(argc, argv)  ;
     iki_set_sv_type_file_path_name("xsim.dir/Top_behav/xsim.svtype");
     iki_set_crvs_dump_file_path_name("xsim.dir/Top_behav/xsim.crvsdump");
     void* design_handle = iki_create_design("xsim.dir/Top_behav/xsim.mem", (void *)relocate, (void *)sensitize, (void *)simulate, (void*)0, 0, isimBridge_getWdbWriter(), 0, argc, argv);
